9571778437219615936b3c97c6143f5db053d8cf
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-08-01 09:21:29 +0300

    first commit

---
+++ app/src/main/java/dev/queenter/myposts/ApiInterface.kt
@@ -0,0 +1,9 @@
+package dev.queenter.myposts
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiInterface {
+    @GET("/posts")
+    fun getPost():Call<List<Post>>
+}
---
+++ app/src/main/java/dev/queenter/myposts/MainActivity.kt
@@ -0,0 +1,35 @@
+package dev.queenter.myposts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fetchPosts()
+    }
+    fun fetchPosts(){
+        var apiClient = ApiClient.buildApiClient(ApiInterface::class.java)
+        var request = apiClient.getPost()
+
+        request.enqueue(object : Callback<List<Post>> {
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                if (response.isSuccessful){
+                    var posts = response.body()
+                    Toast.makeText(baseContext, "${posts!!.size} posts", Toast.LENGTH_LONG).show()
+
+                }
+            }
+
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+
+            }
+
+        })
+    }
+}
